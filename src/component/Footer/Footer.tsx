e47260c13fd72e3b424152f42bd279691436153f
1985mendo <1985mendo@gmail.com>
2023-02-19 11:34:05 +0900

    signout追加

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { chakra, Container } from '@chakra-ui/react'
 export const Footer = () => {
   return (
     <chakra.footer py={4} bgColor={'blue.600'} color={'white'}>
-      <Container maxW={'container.lg'}>Footer</Container>
+      <Container maxW={'container.lg'}>Inc.</Container>
     </chakra.footer>
   )
 }
