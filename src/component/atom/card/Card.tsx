7c81c6802164bf4cfe127985242088b6fe5c972e
1985mendo <1985mendo@gmail.com>
2023-02-20 03:29:49 +0900

    UserCard追加

---
+++ src/component/atom/card/Card.tsx
@@ -0,0 +1,11 @@
+import { Avatar, Box, Stack, Text } from "@chakra-ui/react"
+
+export const Card = (props: any) => {
+  const { user } = props
+  return (
+    <>
+    
+  </>
+  )
+}
+export default Card
--- src/organisms/user/UserCard.tsx
+++ src/organisms/user/UserCard.tsx
@@ -5,37 +5,36 @@ export const UserCard = (props: any) => {
   const { user } = props
   return (
     <>
-    <Avatar 
-      src={user.image}
-      height={120}
-      width={120}
-      marginLeft={150}
-      marginTop={30}
-    />
-    <Box
-      display="flex"
-      alignItems="start"
-      justifyContent="center"
-      height="50vh"
-    >
-      <Stack spacing={4} marginTop={30}>
-      <Stack direction="row" spacing={78}>
-        <Text fontWeight="bold">名 前</Text>
-        <Text>{user.name}</Text>
+      <Avatar 
+        height={120}
+        width={120}
+        marginLeft={150}
+        marginTop={30}
+      />
+      <Box
+        display="flex"
+        alignItems="start"
+        justifyContent="center"
+        height="50vh"
+      >
+        <Stack spacing={4} marginTop={30}>
+        <Stack direction="row" spacing={78}>
+          <Text fontWeight="bold">名 前</Text>
+          <Text>{user.name}</Text>
+        </Stack>
+        <Stack direction="row" spacing={50}>
+          <Text fontWeight="bold">社員番号</Text>
+          <Text>{user.employeeId}</Text>
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <Text fontWeight="bold">メールアドレス</Text>
+          <Text>{user.email}</Text>
+        </Stack>
+        <Stack direction="row" spacing={9}>
+          <Text fontWeight="bold">担当エリア</Text>
+          <Text>{user.area}</Text>
+        </Stack>
       </Stack>
-      <Stack direction="row" spacing={50}>
-        <Text fontWeight="bold">社員番号</Text>
-        <Text>{user.employeeId}</Text>
-      </Stack>
-      <Stack direction="row" spacing={1}>
-        <Text fontWeight="bold">メールアドレス</Text>
-        <Text>{user.email}</Text>
-      </Stack>
-      <Stack direction="row" spacing={9}>
-        <Text fontWeight="bold">担当エリア</Text>
-        <Text>{user.area}</Text>
-      </Stack>
-    </Stack>
     </Box>
     </>
   )
