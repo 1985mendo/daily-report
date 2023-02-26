471109ea1292c502635671d779b35073c52c3960
1985mendo <1985mendo@gmail.com>
2023-02-26 15:53:03 +0900

    Footer伸縮調整

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import {  Box, chakra, Container, Flex, Link } from '@chakra-ui/react'
+import { Box, chakra, Container, Flex, Link } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 const Footer = () => {
@@ -10,22 +10,38 @@ const Footer = () => {
     router.push('/signup')
   }
 
-  
   return (
     <Box pb={50}>
-      <chakra.footer 
-        py={4} 
-        bgColor={'blue.600'} 
+      <chakra.footer
+        py={4}
+        bgColor={'blue.600'}
         color={'white'}
         position={`fixed`}
         bottom={0}
-        width={`100%`} 
-        h={'50px'}  
+        width={`100%`}
+        h={'50px'}
       >
         <Container maxW={'container.lg'}>
-          <Flex flexDirection={'column'} gap={2} alignItems={'start'}>
-              <Link lineHeight={1} onClick={onClickSignin}>サインイン</Link>
-              <Link lineHeight={1} onClick={onClickSignup}>新規登録</Link>
+          <Flex
+            flexDirection={'column'}
+            gap={2}
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={'100%'}
+            h={'100%'}
+          >
+            <Flex width={'100%'} justifyContent={'center'}>
+              <Box>
+                <Link lineHeight={1} onClick={onClickSignin}>
+                  サインイン
+                </Link>
+              </Box>
+              <Box ml={2}>
+                <Link lineHeight={1} onClick={onClickSignup}>
+                  新規登録
+                </Link>
+              </Box>
+            </Flex>
           </Flex>
         </Container>
       </chakra.footer>
