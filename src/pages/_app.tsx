57fe10e13d082c688e51b6a865e20c2c2f835fd0
1985mendo <1985mendo@gmail.com>
2023-02-26 15:48:33 +0900

    UserCard下部余白削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider, Stack } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
 import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
 import Header from '@src/component/Header/Header'
 import Footer from '@src/component/Footer/Footer'
 import "@src/global.css"
-
 
 initializeFirebaseApp()
 
@@ -13,9 +12,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AuthProvider>
+        <Box bg={"#aafafc"}>
         <Header />
-        <Component {...pageProps} />
+        <Stack as="main" minHeight="100vh">
+          <Component {...pageProps} />
+        </Stack>
         <Footer />
+        </Box>
       </AuthProvider>
     </ChakraProvider>
   )
